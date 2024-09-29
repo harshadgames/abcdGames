d34ae2f035a37a7f3fa62fbf15321b6616da5d09
harshadk <harshadedugames@gmail.com>
2024-09-29 13:08:46 +0530

    wwww

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Define the base path
 define('ROOT_PATH', __DIR__ . '/');
 
-define('BASE_PATH', 'https://wwww.abcdgames.tech/');
+define('BASE_PATH', 'https://www.abcdgames.tech/');
 
 define ('BLOG_PATH', 'https://blog.abcdgames.tech/')
 ?>
